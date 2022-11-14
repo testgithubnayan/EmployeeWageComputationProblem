91927952b4096804802bd8c8954aee6f80defadf
NAYAN SONI <107099130+testgithubnayan@users.noreply.github.com>
2022-11-14 16:18:07 +0530

    just refactoring the code ..............................

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,12 +2,20 @@ package com.bridgelabz;
 
 public class EmployeeWage {
 
+    static final int WAGE_PER_HOUR = 20;
     static final int FULL_DAY_HOUR = 8;
     static final int PART_TIME__HOUR = 4;
     static final int IS_FULL_TIME = 2;
     static final int IS_PART_TIME = 1;
+    static final int WORKING_DAY_PER_MONTH = 20;
+    static final int WORKING_HOUR_PER_MONTH = 100;
 
-    public static int computeEmpWage(String company, int WAGE_PER_HOUR, int WORKING_DAY_PER_MONTH, int WORKING_HOUR_PER_MONTH) {
+    public static void main(String[] args) {
+        System.out.println("Welcome to employee wage computation program");
+        computeEmpWage();
+    }
+
+    public static int computeEmpWage() {
         int empPresent = (int) (Math.floor(Math.random() * 10) % 3);
         int dayCount = 1;
         int workingHours = 0;
@@ -34,15 +42,7 @@ public class EmployeeWage {
             dayCount++;
         }
         System.out.println("working Hours: " + workingHours);
-        System.out.println("Total wage for company: " + company + " is: " + totalWage);
+        System.out.println("Total wage: " + totalWage);
         return totalWage;
-    }
-
-    public static void main(String[] args) {
-        System.out.println("Welcome to employee wage computation");
-        computeEmpWage("goodrick", 20, 2, 10 );
-        computeEmpWage("ajio", 10, 4, 20 );
-        computeEmpWage("Reliance", 10, 3, 10 );
-        computeEmpWage("Flipcart", 15, 5, 30 );
     }
 }
